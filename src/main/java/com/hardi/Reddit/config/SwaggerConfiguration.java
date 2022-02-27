7b3f88fdd18fadd56305a242d5f266430af34256
sasahardi997 <sasahardi@gmail.com>
2022-02-27 13:08:44 +0100

    swagger implemented

---
+++ src/main/java/com/hardi/Reddit/config/SwaggerConfiguration.java
@@ -0,0 +1,37 @@
+package com.hardi.Reddit.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfiguration {
+
+    @Bean
+    public Docket redditApi(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(getaApiInfo());
+    }
+
+    private ApiInfo getaApiInfo() {
+        return new ApiInfoBuilder()
+                .title("Reddit API")
+                .version("1.0")
+                .description("API form Reddit Application")
+                .contact(new Contact("Alekasndar Hardi", "sasahardi@gmail.com", "sasahardi@gmail.com"))
+                .license("Apache License Version 2.0")
+                .build();
+    }
+}
